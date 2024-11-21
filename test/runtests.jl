6e4a4a672ce3fd597e8ea6359507b6aa8d709e81
Kristoffer Carlsson <kcarlsson89@gmail.com>
2024-11-21 13:59:54 +0100

    Set up CI
    (cherry picked from commit 56d561c22e1ab8e0421160edbdd42f3f194ecfa8)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
 for file in readlines(joinpath(@__DIR__, "testgroups"))
+    @info "Testing $file"
     include(file * ".jl")
 end
